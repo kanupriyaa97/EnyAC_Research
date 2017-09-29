9e7fed675849f6df2828e1338813258cdbed58cc
kanupriyaa97 <kanupriyaa_star@yahoo.com>
2017-09-29 08:15:31 +0300

    Skeleton for file

--- source/linear_reg.py
+++ source/linear_reg.py
@@ -1,43 +1,46 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+#HOW TO ADD ALL THESE LIBRARIES?????????????    
 
-# Load the diabetes dataset
-diabetes = datasets.load_diabetes()
+disch_data = pd.read_csv('G_disch_20160728.csv', header = [10,11,12]) #COL 12 WHICH IS THE RESULTS IS ALSO A FUNCTION OF ITS PREVIOUS VALUES.
+gage_data = pd.read_csv('G_gage_20160728.csv', header = [10,11,12]) 
 
+disch_res = pd.read_csv('G_disch_20160728.csv', header = [12])
 
-# Use only one feature
-diabetes_X = diabetes.data[:, np.newaxis, 2]
+data = disch_data + gage_data #HOW TO COMBINE BOTH THE DATA SETS?????
 
 # Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
+data_train = data[:-20]
+data_test = data[-20:]
 
 # Split the targets into training/testing sets
-diabetes_y_train = diabetes.target[:-20]
-diabetes_y_test = diabetes.target[-20:]
+res_train = disch_res[:-20]
+res_test = disch_res[-20:]
 
 # Create linear regression object
 regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
+regr.fit(data_train, res_train)
 
 # Make predictions using the testing set
-diabetes_y_pred = regr.predict(diabetes_X_test)
+res_pred = regr.predict(data_test)
 
 # The coefficients
 print('Coefficients: \n', regr.coef_)
 # The mean squared error
 print("Mean squared error: %.2f"
-      % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+      % mean_squared_error(res_test, res_pred))
 # Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
+print('Variance score: %.2f' % r2_score(res_test, res_pred))
+
 
 # Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
+plt.scatter(data_test, res_test,  color='black')
+plt.plot(data_test, res_pred, color='blue', linewidth=3)
 
 plt.xticks(())
 plt.yticks(())
