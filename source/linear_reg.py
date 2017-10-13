bfa09a57b80b2c0292a7f2f7e57322ee009fa670
kanupriyaa97 <kanupriyaa_star@yahoo.com>
2017-10-13 08:34:42 +0300

    Working script except plots

--- source/linear_reg.py
+++ source/linear_reg.py
@@ -3,45 +3,63 @@ import pandas as pd
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
-#HOW TO ADD ALL THESE LIBRARIES?????????????    
+from sklearn.linear_model import LinearRegression
+
+def clean_dataset(df):
+    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
+    df.dropna(inplace=True)
+    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    return df[indices_to_keep].astype(np.float64)
+
 
 #Assuming that dependant variable (disch row 12) is not dependant on its previous values.
 disch_data = pd.read_csv('G_disch_20160728.csv')[9:11] 
 gage_data = pd.read_csv('G_gage_20160728.csv')[9:12]
 
-disch_res = pd.read_csv('G_disch_20160728.csv')[11:12]
+disch_res = pd.read_csv('G_disch_20160728.csv')[10:11]
 
 data = pd.concat([disch_data, gage_data]) 
 
 # Split the data into training/testing sets
-data_train = data[:-20]
-data_test = data[-20:]
+data_train = clean_dataset(data.ix[:,:-20].transpose())
+data_test = clean_dataset(data.ix[:,-20:-4].transpose())
+
+#print(data_train.ix[:5,:])
+#print(data_test)
 
 # Split the targets into training/testing sets
-res_train = disch_res[:-20]
-res_test = disch_res[-20:]
+res_train = clean_dataset(disch_res.ix[:, 3:-17].transpose())
+res_test = clean_dataset(disch_res.ix[:, -17:].transpose())
+
+#print(res_train)
+#print(res_test)
+
+#print(data.ix[:,0:5])
+#print(type(data.ix[:,0:5]))
+#print(data_train)
 
 # Create linear regression object
-regr = linear_model.LinearRegression()
+regr = LinearRegression()
 
 # Train the model using the training sets
 regr.fit(data_train, res_train)
-
+LinearRegression(copy_X = True, fit_intercept = True, normalize = False)
 # Make predictions using the testing set
 res_pred = regr.predict(data_test)
 
 # The coefficients
 print('Coefficients: \n', regr.coef_)
 # The mean squared error
-print("Mean squared error: %.2f"
-      % mean_squared_error(res_test, res_pred))
+print("Mean squared error: %.2f"% mean_squared_error(res_test, res_pred))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % r2_score(res_test, res_pred))
 
 
 # Plot outputs
-plt.scatter(data_test, res_test,  color='black')
-plt.plot(data_test, res_pred, color='blue', linewidth=3)
+print(data_test.shape)
+print(res_test.shape)
+plt.scatter(res_test, res_pred,  color='black')
+plt.plot(data_test, res_pred, color='blue', linewidth=1)
 
 plt.xticks(())
 plt.yticks(())
